623a2e4596d04211750b411d0a9211499d55c5f7
Pliszka <karoliniakkjk@gmail.com>
2022-02-04 15:43:59 +0100

    basic background

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+root = Tk()
+root.title('Password Manager')
+root.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+padlock = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=padlock)
+canvas.grid(row=1, column=1)
+
+
+
+
+
+root.mainloop()
